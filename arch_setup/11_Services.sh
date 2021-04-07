418f72d01d78b7b27070bc8a35ffa378cc43bc8a
Jonny Petraglia <jpetraglia@inspirato.com>
2021-04-07 10:48:43 -0600

    Added more Services to enable

--- arch_setup/11_Services.sh
+++ arch_setup/11_Services.sh
@@ -1,2 +1,21 @@
 systemctl enable sddm.service
 systemctl enable sshd.service
+systemctl enable bluetooth.service
+
+
+## TODO: echo to /etc/pulse/system.pa
+#.ifexists module-bluetooth-policy.so
+#load-module module-bluetooth-policy
+#.endif
+
+#.ifexists module-bluetooth-discover.so
+#load-module module-bluetooth-discover
+#.endif
+
+#.ifexists module-bluez5-device.so
+#load-module module-bluez5-device
+#.endif
+
+#.ifexists module-bluez5-discover.so
+#load-module module-bluez5-discover
+#.endif
