330a98a2c482e18b279c1761763574dfdd57ffe5
Jonny Petraglia <jpetraglia@inspirato.com>
2021-05-07 10:58:50 -0600

    Added sfdx package export

--- export_installed_packages.sh
+++ export_installed_packages.sh
@@ -4,4 +4,4 @@ pacman_installed > packages_(pretty_date)_pacman.txt
 npm_installed > packages_(pretty_date)_npm.txt
 gem_installed > packages_(pretty_date)_gem.txt
 fisher list > packages_(pretty_date)_fish.txt
-
+sfdx_installed > packages_(pretty_date)_sfdx.txt
