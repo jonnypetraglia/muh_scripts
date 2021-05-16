5b24540f0d6047759aa97c3b56388b6b847f89cf
Jonny Petraglia <jpetraglia@inspirato.com>
2021-05-16 08:43:53 -0600

    Updated dependency lists

--- arch_setup/10_Packages.sh
+++ arch_setup/10_Packages.sh
@@ -1,5 +1,6 @@
 sudo -u $MY_USERNAME yay --noconfirm --needed -S $(cat ./packages_*_pacman.txt)
+cat ./packages_*_fish.txt | fish install
 npm install --global $(cat ./packages_*_npm.txt)
 sudo -u $MY_USERNAME gem install $(cat ./packages_*_gem.txt)
-cat ./packages_*_fish.txt | fish install
+sudo pip install $(cat ./packages_*_pip.txt)
 echo y | sfdx plugin:install $(cat ./packages_*_sfdx.txt)
