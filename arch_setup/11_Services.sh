5c63a71df73c28f419baff6ac50b0073ae45f532
Jonny Petraglia <jpetraglia@inspirato.com>
2022-01-22 10:16:44 -0700

    Penultimate Syncthing change

--- arch_setup/11_Services.sh
+++ arch_setup/11_Services.sh
@@ -6,6 +6,13 @@ systemctl enable reflector.service
 
 ## TODO Enable Syncthing.service from muh_dotfiles
 
+# If root
+echo 1000000 > /proc/sys/fs/inotify/max_user_watches
+echo "fs.inotify.max_user_watches=1000000" >> /etc/sysctl.d/90-override.conf
+# If not root:
+## echo 804800 | sudo tee /proc/sys/fs/inotify/max_user_watches
+## echo "fs.inotify.max_user_watches=1000000" | sudo tee -a /etc/sysctl.d/90-override.conf
+
 
 ## TODO: echo to /etc/pulse/system.pa
 #.ifexists module-bluetooth-policy.so
