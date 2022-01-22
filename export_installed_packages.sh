6f0b70e4a19cc2d13eab38209fe9c9024386eba5
Jonny Petraglia <jpetraglia@inspirato.com>
2022-01-22 09:00:04 -0700

    Updated dependency lists

--- export_installed_packages.sh
+++ export_installed_packages.sh
@@ -6,3 +6,4 @@ npm_installed > packages_(pretty_date)_npm.txt
 gem_installed > packages_(pretty_date)_gem.txt
 pip_installed > packages_(pretty_date)_pip.txt
 sfdx_installed > packages_(pretty_date)_sfdx.txt
+code --list-extensions > packages_(pretty_date)_vscode.txt
